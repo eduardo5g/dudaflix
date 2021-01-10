5e81fa932729c15773a6aa40dcdef73d9ce77d65
Eduardo Ferreira <localsoft@hotmail.com>
2021-01-09 21:57:50 -0300

    delevoping

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\{
+    PlayController
+};
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Video;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,6 +17,20 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/play', [PlayController::class,'index']);
+
+Route::get('/catalog', function () {
+    $users = User::all();
+    $categories = Category::all();
+    $_video = new Video();
+    foreach($users as $user){
+        $video= $_video->filter('user',$user->name);
+
+        $videos[$user->name]=(object) $video;
+    }
+    return view('index',compact('users','categories','videos'));
+});
 
 Route::get('/', function () {
     return view('welcome');
